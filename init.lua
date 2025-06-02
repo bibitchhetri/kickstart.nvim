78b202c316ff667e9099f823abd8229ed3cbb7a0
bibitchhetri <kunwar.bibit7@gmail.com>
2025-06-02 14:11:22 +0545

    Initial custom kickstart.nvim config

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -593,11 +593,11 @@ require('lazy').setup({
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
+            -- vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            --   buffer = event.buf,
+            --   group = highlight_augroup,
+            --   callback = vim.lsp.buf.document_highlight,
+            -- })
 
             vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
               buffer = event.buf,
@@ -671,10 +671,12 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
+        ts_ls = {},
+        jdtls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -894,7 +896,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'habamax'
     end,
   },
 
@@ -944,7 +946,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'dart' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -976,15 +978,15 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,144 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Copilot plugin setup
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = '<Tab>',
+            accept_word = '<C-Right>',
+            accept_line = '<C-Down>',
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
+          },
+        },
+        panel = { enabled = false },
+      }
+    end,
+  },
+
+  -- Alpha dashboard setup
+  {
+    'goolord/alpha-nvim',
+    config = function()
+      require('alpha').setup(require('alpha.themes.startify').config)
+    end,
+  },
+
+  -- Undotree plugin setup
+  {
+    'mbbill/undotree',
+    config = function()
+      vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR>', { noremap = true, silent = true })
+    end,
+  },
+
+  -- Harpoon plugin setup
+  {
+    'ThePrimeagen/harpoon',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local mark = require 'harpoon.mark'
+      local ui = require 'harpoon.ui'
+
+      vim.keymap.set('n', '<leader>a', function()
+        mark.add_file()
+      end, { desc = 'Add file to Harpoon' })
+
+      vim.keymap.set('n', '<leader>h', function()
+        ui.toggle_quick_menu()
+      end, { desc = 'Toggle Harpoon menu' })
+
+      vim.keymap.set('n', '<C-h>', function()
+        ui.nav_file(1)
+      end, { desc = 'Go to Harpoon file 1' })
+      vim.keymap.set('n', '<C-j>', function()
+        ui.nav_file(2)
+      end, { desc = 'Go to Harpoon file 2' })
+      vim.keymap.set('n', '<C-k>', function()
+        ui.nav_file(3)
+      end, { desc = 'Go to Harpoon file 3' })
+      vim.keymap.set('n', '<C-l>', function()
+        ui.nav_file(4)
+      end, { desc = 'Go to Harpoon file 4' })
+    end,
+  },
+
+  -- Trouble plugin setup
+  {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('trouble').setup {
+        auto_open = false,
+        auto_close = false,
+        auto_preview = true,
+        auto_fold = false,
+        signs = {
+          error = 'E',
+          warning = 'W',
+          hint = 'H',
+          information = 'I',
+          other = '?',
+        },
+      }
+    end,
+  },
+
+  -- Flutter tools
+  {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional UI improvements
+      'mfussenegger/nvim-dap', -- for debugging if needed
+    },
+    config = function()
+      require('flutter-tools').setup {
+        flutter_path = '/opt/homebrew/bin/flutter', -- full path to flutter
+        widget_guides = { enabled = true },
+        closing_tags = { highlight = 'Comment' },
+        lsp = {
+          color = { enabled = true },
+          on_attach = function(_, bufnr)
+            local bufopts = { noremap = true, silent = true, buffer = bufnr }
+            vim.keymap.set('n', '<leader>fr', '<cmd>FlutterRun<cr>', bufopts)
+            vim.keymap.set('n', '<leader>fh', '<cmd>FlutterHotReload<cr>', bufopts)
+            vim.keymap.set('n', '<leader>fq', '<cmd>FlutterQuit<cr>', bufopts)
+            vim.keymap.set('n', '<leader>fl', '<cmd>FlutterLog<cr>', bufopts) -- optional log key
+          end,
+        },
+      }
+    end,
+    ft = { 'dart' },
+  },
+
+  -- Toggleterm for running commands (optional)
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = true,
+    cmd = { 'ToggleTerm', 'TermExec' },
+  },
+
+  -- Visual Multi plugin setup
+  {
+    'mg979/vim-visual-multi',
+    branch = 'master',
+    keys = { '<C-n>', '\\A' }, -- load only when these keys are used
+    init = function()
+      vim.g.VM_default_mappings = 1
+    end,
+  },
+}
